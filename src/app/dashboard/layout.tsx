aeaeaea7f2b6040e9f36ffdd106d1316c1761bcb
Prabal-verma <122899996+Prabal-verma@users.noreply.github.com>
2024-06-22 06:20:48 +0530

    dashboard sorted

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Inter } from "next/font/google";
+import Header from './_components/Header';
 
 const inter = Inter({ subsets: ["latin"] });
 export default function DashboardLayout({
@@ -8,10 +9,15 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <div>
+        <Header/>
+        <div className='mx-5 md:mx-20 lg:mx-36'>
+        {children}
+        </div>
+       
+    </div>
+  
+    
   );
 }
 
